e74adc3944962de9c59ec3f5ec162d5a69c0e0e0
Jerlin shabi <138958887+jerlin-shabi@users.noreply.github.com>
2023-09-24 20:14:16 +0530

    Create 1st.py

---
+++ 1st.py
@@ -0,0 +1,15 @@
+python
+import pygame
+
+# Initialize Pygame
+pygame.init()
+
+# Set the window size
+window_width = 800
+window_height = 600
+
+# Create the game window
+game_window = pygame.display.set_mode((window_width, window_height))
+
+# Set the window title
+pygame.display.set_caption("Bubble Game")
